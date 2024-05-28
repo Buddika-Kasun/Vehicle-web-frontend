d7d5237964e70e5413728b70661d6866b09b3830
Buddika-kasun <buddikakasun80@gmail.com>
2024-05-28 12:55:20 +0530

    dashboard sales chart request and fetch data part

--- src/views/admin/Dashboard/TotalSalesChart.jsx
+++ src/views/admin/Dashboard/TotalSalesChart.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const data = [
@@ -53,6 +54,26 @@ const data = [
 ]
 
 export default function TotalSalesChart() {
+
+    // 
+
+    const [ data1, setData1] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Update the URL to your specific API endpoint for fetching data
+                const response = await axios.get('http://localhost:5062/api/SalesChart')
+                setData1(response.data) // Assume the response data is the array of data
+            } catch (error) {
+                console.error('Failed to fetch data:', error)
+            }
+        }
+        fetchData()
+    }, [])
+
+    // 
+
     return (
         <div className="h-[28rem] bg-white p-4 rounded-sm border border-gray-200 flex flex-col flex-1">
             <strong className="text-gray-700 font-medium">Total Sales</strong>
@@ -61,7 +82,7 @@ export default function TotalSalesChart() {
                     <BarChart
                         width={500}
                         height={300}
-                        data={data}
+                        data={data1}
                         margin={{
                             top: 20,
                             right: 10,
